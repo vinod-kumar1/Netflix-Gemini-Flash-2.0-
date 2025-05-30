9fe02001edf23a52c5a5d2228eeddf35630b85b0
vinod-kumar1 <vinu22149@gmail.com>
2025-05-30 21:37:05 +0530

    added pagiination loader

--- src/Components/MovieCategories.js
+++ src/Components/MovieCategories.js
@@ -7,17 +7,16 @@ import {
   setPlayingMovieDetails,
 } from "../utils/moviesPagination";
 import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
 import { fetchMovieKey } from "../utils/fetch";
 
 export function MovieCategoryList({ name, movies, type }) {
   let dispatch = useDispatch();
+  let [loading, setLoading] = useState(false);
 
   function findMovieKeyAndSet(id) {
     fetchMovieKey(id)
       .then((res) => res.json())
       .then((json) => {
-        console.log("json", json);
         dispatch(setPlaying(json.results[0]));
         window.scroll({
           top: 0,
@@ -26,6 +25,10 @@ export function MovieCategoryList({ name, movies, type }) {
       })
       .catch(console.log);
   }
+
+  useEffect(() => {
+    setLoading(false);
+  }, [movies]);
 
   return (
     <div className="w-screen px-4 overflow-x-hidden mb-5">
@@ -48,6 +51,7 @@ export function MovieCategoryList({ name, movies, type }) {
         ))}
         <button
           onClick={() => {
+            setLoading(true);
             let temp = type.split(" ").join("_").toLowerCase();
             dispatch(
               setTypePageCount({
@@ -59,96 +63,90 @@ export function MovieCategoryList({ name, movies, type }) {
           className="font-mono text-2xl h-50 px-2 bg-red-500 text-white rounded-r-md hover:bg-white hover:text-red-500 cursor-pointer hover:border-[0.5px] mr-2"
         >
           {">"}
+          {loading && <p class="animate-spin text-xl">🌀</p>}
         </button>
       </div>
     </div>
   );
 }
 
-export default function MovieCategories() {
-  let [categories, setCategories] = useState({ morePopular: [], popular: [] });
-  let [popupModel, setPopUpModel] = useState({});
-  let model = useRef();
-  let popUpMovie = useRef();
-  let movies = useSelector((state) => state.moviesPagn.movies);
-  let page = useSelector((state) => state.moviesPagn.page);
-  let pages = useSelector((state) => state.moviesPagn.totalPages);
-  let dispatch = useDispatch();
+// export default function MovieCategories() {
+//   let [categories, setCategories] = useState({ morePopular: [], popular: [] });
+//   let [popupModel, setPopUpModel] = useState({});
+//   let model = useRef();
+//   let popUpMovie = useRef();
+//   let movies = useSelector((state) => state.moviesPagn.movies);
+//   let page = useSelector((state) => state.moviesPagn.page);
+//   let pages = useSelector((state) => state.moviesPagn.totalPages);
+//   let dispatch = useDispatch();
 
-  useEffect(() => {
-    let popularityLessThan20 = movies.filter((movie) => movie.popularity <= 20);
-    let popularityMorethan20 = movies.filter((movie) => movie.popularity > 20);
-    setCategories({
-      morePopular: popularityMorethan20,
-      popular: popularityLessThan20,
-    });
+//   useEffect(() => {
+//     let popularityLessThan20 = movies.filter((movie) => movie.popularity <= 20);
+//     let popularityMorethan20 = movies.filter((movie) => movie.popularity > 20);
+//     setCategories({
+//       morePopular: popularityMorethan20,
+//       popular: popularityLessThan20,
+//     });
 
-    return () => setCategories({});
-  }, [movies]);
+//     return () => setCategories({});
+//   }, [movies]);
 
-  useEffect(() => {
-    if (popUpMovie.current) {
-      window.scroll({ top: 80, behavior: "smooth" });
-    }
-  }, [popupModel]);
+//   useEffect(() => {
+//     if (popUpMovie.current) {
+//       window.scroll({ top: 80, behavior: "smooth" });
+//     }
+//   }, [popupModel]);
 
-  return (
-    <div className="transition-all duration-500">
-      {popupModel.key && (
-        <div className="absolute top-0" key={`${popupModel.key}-main`}>
-          <dialog open ref={model}>
-            {/* <iframe
-              ref={popUpMovie}
-              allowFullScreen={true}
-              className="w-screen relative h-screen z-99"
-              src={`https://www.youtube.com/embed/${popupModel.key}?autoplay=1&origin=https%3A%2F%2Fwww.themoviedb.org&hl=en&fs=1&autohide=1&mute=1&color=red&loop=1&playlist=${popupModel.key}`}
-            ></iframe> */}
-            <button
-              onClick={() => {
-                model.current.close();
-                setPopUpModel({});
-              }}
-              className="text-2xl text-white z-199 absolute bg-red-500 right-4 top-25 cursor-pointer rounded-[100px] rotate-90 p-3 hover:bg-white hover:text-red-500 hover:shadow-[2px_0px_10px_white]"
-            >
-              <abbr title="close" className="no-underline">
-                X
-              </abbr>
-            </button>
-          </dialog>
-        </div>
-      )}
-      <div className="flex">
-        <div className="*:rounded-4xl flex flex-col w-screen items-center mt-4">
-          <MovieCategoryList
-            setPopUpModel={setPopUpModel}
-            name={"Popular Movies"}
-            moviesList={categories.morePopular}
-          />
-          <p>
-            Total Pages : {page} / {pages}
-          </p>
-          <MovieCategoryList
-            setPopUpModel={setPopUpModel}
-            name={"Silver Movies"}
-            moviesList={categories.popular}
-          />
-        </div>
-        <button
-          disabled={page >= pages}
-          //   onClick={() => dispatch(addPage())}
-          className="bg-red-500 h-max translate-y-15 -translate-x-20 py-2 px-2 rounded-r-lg w-max text-white hover:bg-white hover:text-red-500 hover:border-red-500 hover:border-[0.5px] cursor-pointer"
-        >
-          {[..."Load More Movies"].map((char, i) =>
-            char == " " ? (
-              <br key={i} />
-            ) : (
-              <p key={i} className="px-2">
-                {char}
-              </p>
-            )
-          )}
-        </button>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="transition-all duration-500">
+//       {popupModel.key && (
+//         <div className="absolute top-0" key={`${popupModel.key}-main`}>
+//           <dialog open ref={model}>
+//             <button
+//               onClick={() => {
+//                 model.current.close();
+//                 setPopUpModel({});
+//               }}
+//               className="text-2xl text-white z-199 absolute bg-red-500 right-4 top-25 cursor-pointer rounded-[100px] rotate-90 p-3 hover:bg-white hover:text-red-500 hover:shadow-[2px_0px_10px_white]"
+//             >
+//               <abbr title="close" className="no-underline">
+//                 X
+//               </abbr>
+//             </button>
+//           </dialog>
+//         </div>
+//       )}
+//       <div className="flex">
+//         <div className="*:rounded-4xl flex flex-col w-screen items-center mt-4">
+//           <MovieCategoryList
+//             setPopUpModel={setPopUpModel}
+//             name={"Popular Movies"}
+//             moviesList={categories.morePopular}
+//           />
+//           <p>
+//             Total Pages : {page} / {pages}
+//           </p>
+//           <MovieCategoryList
+//             setPopUpModel={setPopUpModel}
+//             name={"Silver Movies"}
+//             moviesList={categories.popular}
+//           />
+//         </div>
+//         <button
+//           disabled={page >= pages}
+//           className="bg-red-500 h-max translate-y-15 -translate-x-20 py-2 px-2 rounded-r-lg w-max text-white hover:bg-white hover:text-red-500 hover:border-red-500 hover:border-[0.5px] cursor-pointer"
+//         >
+//           {[..."Load More Movies"].map((char, i) =>
+//             char == " " ? (
+//               <br key={i} />
+//             ) : (
+//               <p key={i} className="px-2">
+//                 {char}
+//               </p>
+//             )
+//           )}
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
